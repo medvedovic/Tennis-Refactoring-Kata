18838c0b7d7594cfc53be31a873c8ddb23aab136
medvedovic <medvedovic115@gmail.com>
2020-04-10 20:07:33 +0200

    Invert condition for better future readability

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -19,10 +19,10 @@ namespace Tennis
             if (IsCurrentScoreSame())
                 return IsDeuce() ? "Deuce" : $"{indexedTextualScore[p1]}-All";
 
-            if (p1 < 4 && p2 < 4)
-                return $"{indexedTextualScore[p1]}-{indexedTextualScore[p2]}";
-            else
+            if (p1 >= 4 || p2 >= 4)
                 return ((p1 - p2) * (p1 - p2) == 1) ? $"Advantage {GetLeadingPlayerName()}" : $"Win for {GetLeadingPlayerName()}";
+            else
+                return $"{indexedTextualScore[p1]}-{indexedTextualScore[p2]}";
         }
 
         private string GetLeadingPlayerName() => p1 > p2 ? p1N : p2N;
