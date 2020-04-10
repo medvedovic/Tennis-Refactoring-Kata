54c25d3238e7d8daa260f8cd09094638a7dfd3c2
medvedovic <medvedovic115@gmail.com>
2020-04-10 18:42:13 +0200

    Extract condition to a named method

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -18,15 +18,19 @@ namespace Tennis
         {
             string s;
             if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
-                return (p1 == p2) ? $"{indexedTextualScore[p1]}-All" : $"{indexedTextualScore[p1]}-{indexedTextualScore[p2]}";
+                return IsCurrentScoreSame()
+                    ? $"{indexedTextualScore[p1]}-All"
+                    : $"{indexedTextualScore[p1]}-{indexedTextualScore[p2]}";
             else
             {
-                if (p1 == p2)
+                if (IsCurrentScoreSame())
                     return "Deuce";
                 s = p1 > p2 ? p1N : p2N;
                 return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
             }
         }
+
+        private bool IsCurrentScoreSame() => p1 == p2;
 
         public void WonPoint(string playerName)
         {
