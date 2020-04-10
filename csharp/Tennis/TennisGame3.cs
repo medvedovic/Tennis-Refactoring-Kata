084e9c16d42972ebc608a5f14825bf9e7478afc5
medvedovic <medvedovic115@gmail.com>
2020-04-10 20:17:43 +0200

    Rename dumbly named fields

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -4,26 +4,26 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        private int player1Points;
+        private int player2Points;
+        private readonly string player1Name;
+        private readonly string player2Name;
         private static readonly string[] indexedTextualScore = { "Love", "Fifteen", "Thirty", "Forty" };
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
         }
 
         public string GetScore()
         {
             if (IsCurrentScoreSame())
-                return IsDeuce() ? "Deuce" : $"{indexedTextualScore[p1]}-All";
+                return IsDeuce() ? "Deuce" : $"{indexedTextualScore[player1Points]}-All";
 
             return HasAnyPlayerReachedFourPoints()
                 ? GetTextualScoreForAdvantageOrWin()
-                : $"{indexedTextualScore[p1]}-{indexedTextualScore[p2]}";
+                : $"{indexedTextualScore[player1Points]}-{indexedTextualScore[player2Points]}";
         }
 
         private string GetTextualScoreForAdvantageOrWin()
@@ -31,22 +31,22 @@ namespace Tennis
             ? $"Advantage {GetLeadingPlayerName()}"
             : $"Win for {GetLeadingPlayerName()}";
 
-        private int GetCurrentScoreDifference() => p1 - p2;
+        private int GetCurrentScoreDifference() => player1Points - player2Points;
 
-        private bool HasAnyPlayerReachedFourPoints() => p1 >= 4 || p2 >= 4;
+        private bool HasAnyPlayerReachedFourPoints() => player1Points >= 4 || player2Points >= 4;
 
-        private string GetLeadingPlayerName() => p1 > p2 ? p1N : p2N;
+        private string GetLeadingPlayerName() => player1Points > player2Points ? player1Name : player2Name;
 
-        private bool IsDeuce() => p1 >= 3 && p2 >= 3 && IsCurrentScoreSame();
+        private bool IsDeuce() => player1Points >= 3 && player2Points >= 3 && IsCurrentScoreSame();
 
-        private bool IsCurrentScoreSame() => p1 == p2;
+        private bool IsCurrentScoreSame() => player1Points == player2Points;
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.p1 += 1;
+                this.player1Points += 1;
             else
-                this.p2 += 1;
+                this.player2Points += 1;
         }
 
     }
